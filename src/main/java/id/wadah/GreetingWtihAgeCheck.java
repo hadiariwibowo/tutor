4b0abf9714ee0a9de01a235acdc397431c558245
Hadi Ariwibowo <hadi.ariwibowo@gmail.com>
2025-08-03 20:39:26 +0700

    Menambahkan if-else untuk pembuatan kriteria umur.

--- src/main/java/id/wadah/GreetingWtihAgeCheck.java
+++ src/main/java/id/wadah/GreetingWtihAgeCheck.java
@@ -2,18 +2,22 @@ package id.wadah;
 
 import java.util.Scanner;
 
-public class Greeting {
+public class GreetingWtihAgeCheck {
     public static void main(String[] args)
     {
         String nama;
         int umur;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Masukan nama anda: ");
-        nama = scanner.nextLine();
         System.out.print("Masukan umur anda: ");
         umur = scanner.nextInt(); // Bila ada kondisi dimana nextInt dahulu, maka perlu dipanggil lagi nextLine agar menghabiskan newline sebelumnya.
+        scanner.nextLine();
+        System.out.print("Masukan nama anda: ");
+        nama = scanner.nextLine();
         scanner.close();
-        System.out.printf("Halo, saya %s, umur saya %d tahun.%n", nama, umur);
+        if (umur < 17)
+            System.out.println("Kamu belum dewasa.");
+        else
+            System.out.println("Kamu sudah dewasa.");
     }
 }
