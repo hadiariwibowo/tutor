d04ecf7a31cf0f01d762c1c0b9c6d63a1d5462c3
Hadi Ariwibowo <hadi.ariwibowo@gmail.com>
2025-08-03 19:37:20 +0700

    Menambahkan informasi tambahan terkait Nama, Umur, dan Kota.

--- src/main/java/id/wadah/App.java
+++ src/main/java/id/wadah/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Nama: Hadi");
+        System.out.println("Umur: 48");
+        System.out.println("Kota: Jakarta");
     }
 }
